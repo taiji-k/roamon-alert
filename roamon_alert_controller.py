fca6096c798018f9762ca864f4690dd8b92555c2
TakayukiTomatsuri <TakayukiTomatsuri@users.noreply.github.com>
2020-01-21 17:02:16 +0900

    Add commands to add contact info and to show contact list and to stop daemons. And Change daemon constructor to receive roamon_alert_checker object as argment.

--- roamon_alert_controller.py
+++ roamon_alert_controller.py
@@ -16,29 +16,36 @@ logger = logging.getLogger(__name__)
 # ファイルの保存先
 dir_path_data = "/var/tmp"
 file_path_contact_list = os.path.join(dir_path_data, "contact_list.json")
-# file_path_vrps = os.path.join(dir_path_data, "asnip_vrps.dat")
-# file_path_rib = os.path.join(dir_path_data, "asnip.dat")
+file_path_vrps = os.path.join(dir_path_data, "vrps.dat")
+file_path_rib = os.path.join(dir_path_data, "rib.dat")
+log_path = "/tmp/alertd.log"
+
+pid_file_path = "/var/run/alertd.pid"
 
 # ロゴの描画
 f = Figlet(font='slant')
 print(f.renderText('roamon-alert'))
 
+checker = roamon_alert_watcher.RoamonAlertWatcher(file_path_contact_list, dir_path_data, file_path_vrps, file_path_rib)
+checker.init()
 
 # getサブコマンドの実際の処理を記述するコールバック関数
 def command_add(args):
-    pass
+    checker.add_contact_info_to_list(asn=args.asn, prefix=args.prefix, contact_type=args.type, contact_info=args.dest)
+    # いちいち保存すると遅い？
+    checker.save_contact_list()
 
 
-# 検証サブコマンド　checkのとき呼ばれる関数
+# 連絡先のリストアップ
 def command_list(args):
-    pass
+    checker.print_conatct_lists()
 
 
+# デーモンの開始と終了
 def command_daemon(args):
-    alertd = roamon_alert_daemon.RoamonAlertDaemon("/var/run/alertd.pid")
+    alertd = roamon_alert_daemon.RoamonAlertDaemon(pid_file_path, log_path, checker)
     # 起動
     if args.start:
-        alertd.init("/tmp/alertd.log", "/var/tmp", "/var/tmp/vrps.dat", "/var/tmp/rib.dat", "/var/tmp/contact_list.json")
         alertd.start()
 
     # 終了
@@ -69,10 +76,10 @@ parser_list = subparsers.add_parser('list', help="see `get -h`. It's command to 
 parser_list.set_defaults(handler=command_list)
 
 # daemonコマンドのパーサを作成
-parser_list = subparsers.add_parser('daemon', help="see `get -h`. It's command to control daemon.")
-parser_add.add_argument('--start', action='store_true', help='start daemon (DEFAULT)')
-parser_add.add_argument('--stop', action='store_true', help='stop daemon')
-parser_list.set_defaults(handler=command_daemon)
+parser_daemon = subparsers.add_parser('daemon', help="see `get -h`. It's command to control daemon.")
+parser_daemon.add_argument('--start', action='store_true', help='start daemon (DEFAULT)')
+parser_daemon.add_argument('--stop', action='store_true', help='stop daemon')
+parser_daemon.set_defaults(handler=command_daemon)
 
 # help コマンドの parser を作成
 parser_help = subparsers.add_parser('help', help='see `help -h`')
--- roamon_alert_watcher.py
+++ roamon_alert_watcher.py
@@ -23,10 +23,24 @@ class RoamonAlertWatcher():
         self.rib_data = None
 
 
+    def init(self):
+        # ファイルがない場合は話にならんので作る
+        if not os.path.exists(self.vrps_file_path):
+            self.checker.fetch_vrps_data()
+        if not os.path.exists(self.rib_file_path):
+            self.checker.fetch_rib_data()
+        # 連絡先リストが存在しない場合、適当に作ってセーブ
+        if not os.path.exists(self.contact_list_file_path):
+            self.contact_list = [].append(self.make_contact_info_entry(asn=1899, contact_type="email", contact_info="example1899@example.com") )
+            self.save_contact_list()
+
+        self.load_all_data()
+
     # よく知らないがプログラム終了時に未開放のオブジェクトのデストラクタが呼ばれることは期待できない？
     def __del__(self):
         if len(self.contact_list) > 0:
             self.save_contact_list()
+
 
     # roamon_diffの関数読んでるだけなんでなんとかしたい(roamon_diffをclass化してそっち呼ぶとか？)
     def fetch_rib_data(self):
@@ -44,7 +58,11 @@ class RoamonAlertWatcher():
         self.vrps_data = loaded_db["vrps"]
         self.rib_data = loaded_db["rib"]
 
-
+    def print_conatct_lists(self):
+        for contact_info in self.contact_list:
+            for key, item in contact_info.items():
+                print(item, end="\t")
+            print("")
 
     @classmethod
     def make_contact_info_entry(cls, asn = -1, prefix = "0.0.0.0/0", contact_type = "Default", contact_info = "Default"):
@@ -67,6 +85,10 @@ class RoamonAlertWatcher():
         self.contact_list.append(self.make_contact_info_entry(asn, prefix, contact_type, contact_info))
         return
 
+    # TODO: delete関数の実装
+    def del_contact_info_from_list(s):
+        pass
+
     # TODO: IP Prefix　watchへの対応
     def check_roa_with_all_watched_asn(self):
         logger.debug("start checking")
@@ -88,65 +110,3 @@ class RoamonAlertWatcher():
                     # TODO: Slack送信を実装
                     logger.debug("SEND SLACK MSG TO {} watching ASN: {}".format(contact_info["contact_info"], contact_info["asn"]))
         logger.debug("fin sending msg.")
-    #
-    # def start_daemon(self, contact_list,  work_dir_path):
-    #     rib_file_path = os.path.join(work_dir_path, "rib.dat")
-    #     vrps_file_path = os.path.join(work_dir_path, "vrps.dat")
-    #
-    #     # ここで全データをフェッチする(もしくはdaemon initされたときにフェッチ)
-    #     if not os.path.exists(rib_file_path):
-    #         roamon_diff_getter.fetch_rib_data(work_dir_path, rib_file_path)
-    #     if not os.path.exists(vrps_file_path):
-    #         roamon_diff_getter.fetch_vrps_data(vrps_file_path)
-    #
-    #     # RIBファイル(BGPの)を一定間隔でダウンロードするデーモン起動
-    #     @daemonize.interval_run(60)
-    #     def fetch_rib(work_dir_path, rib_file_path, kws=None):
-    #         print("start daemon to fetch rib")
-    #         roamon_diff_getter.fetch_rib_data(work_dir_path, rib_file_path )
-    #     daemonize.start_daemon(fetch_rib, work_dir_path, rib_file_path, pidpath='/var/run/fetch_rib.pid', logpath='/var/tmp/fetch_rib.log', kws='keyword')
-    #     #
-    #     VRPsファイルを一定間隔でダウンロードするデーモン起動
-    #
-    #     @daemonize.interval_run(60)
-    #     def fetch_vrps(vrps_file_path, kws=None):
-    #         print("start daemon to fetch vrps")
-    #         roamon_diff_getter.fetch_vrps_data(vrps_file_path)
-    #     daemonize.start_daemon(fetch_vrps, vrps_file_path, pidpath='/var/run/fetch_vrps.pid', logpath='/var/tmp/fetch_vrps.log',
-    #                            kws='keyword')
-    #
-    #     # ダウンロードしたファイルをチェックして通知を送るデーモン起動
-    #     @daemonize.do_interval_run(10)
-    #     def check_all_roa(contact_list, vrps_file_path, rib_file_path, kws=None):
-    #         print("start daemon to check ROA and RIB")
-    #         loaded_data = roamon_diff_checker.load_all_data(vrps_file_path, rib_file_path)
-    #         is_valid_list = roamon_diff_checker.check_all_asn_in_vrps(loaded_data["vrps"], loaded_data["rib"])
-    #
-    #         # with open("/var/tmp/temp_mail.log", "w") as f:
-    #         # TODO: IP Prefix　watchへの対応 (けど、IP Prefixだけでwatchって需要ある？ASのBGPオペレータへの通知が主な目的なんだから、ASごとでも十分では ?)
-    #         for contact_info in self.contact_list:
-    #             c_asn = int(contact_info["asn"])
-    #             if c_asn in is_valid_list and not is_valid_list[c_asn]:
-    #                 if contact_info["type"] == "email":
-    #                     # TODO: メール送信を実装
-    #                     print("SEND MAIL TO {} watching ASN: {}".format(contact_info["contact_info"],
-    #                                                                     contact_info["asn"]))
-    #                     # f.writelines("SEND MAIL TO {} watching ASN: {}".format(contact_info["contact_info"], contact_info["asn"]))
-    #     daemonize.start_daemon(check_all_roa, self.contact_list, vrps_file_path, rib_file_path, pidpath='/var/run/check_all_roa.pid', logpath='/var/tmp/check_all_roa.log', kws='keyword')
-    #
-    #
-    #
-    #
-    #     # ここでVRPsを一定時間ごとにfetchして検証するデーモンを起動？
-    #     # ここでBGPを一定時間(2時間)ごとにfetchするデーモンを起動？
-    #     # ここで一定時間ごとにデータをチェックするデーモンを起動？
-    #
-    #
-    # #
-    # # def check_all_roa(self):
-    #
-    #
-    # # TODO: delete関数の実装
-    #
-    # def del_contact_info_from_list(self):
-    #     pass
